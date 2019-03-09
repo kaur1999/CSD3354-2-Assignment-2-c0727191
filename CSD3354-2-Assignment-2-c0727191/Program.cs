d5014924ce38e379ab5158a90a0da5de48165083
visualstudio <visualstudio@122-PC25>
2019-03-08 21:06:38 -0500

    lab15

--- CSD3354-2-Assignment-2-c0727191/Program.cs
+++ CSD3354-2-Assignment-2-c0727191/Program.cs
@@ -10,39 +10,53 @@ using System.Threading.Tasks;
 // March6, 2019
 namespace CSD3354_2_Assignment_2_c0727191
 {
-    public class program
+
+    public class Program
     {
         public static void Main()
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            try
             {
-                DelegateExercises delegateExercises = new DelegateExercises();
                 delegateExercises.Method3();
                 Console.ReadLine();
             }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine("Exception Occurred.");
+                Console.ReadLine();
+                delegateExercises.Method3();
+                Console.ReadLine();
+
+            }
         }
-        public delegate int MyDelegate(out int i);
-        public class DelegateExercises
+    }
+
+
+    public delegate void MyDelegate(ref int intValue);
+
+    public class DelegateExercises
+    {
+
+        void Method1(ref int intValue)
         {
-            int Method1(out int i)
-            {
+            throw new System.Exception();
+            intValue = intValue + 5;
+            System.Console.WriteLine("Method1 " + intValue);
+        }
 
-                i = 100;
-                System.Console.WriteLine("Method1" + i);
-                return 0;
-            }
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                MyDelegate myDelegate1 = null;
-                MyDelegate myDelegate2 = myDelegate + myDelegate1;
-                int intValue;
-                myDelegate2(out intValue);
-            }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
 
-
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intParameter = 5;
+            myDelegate2(ref intParameter);
         }
     }
 }
+
 
 
 
