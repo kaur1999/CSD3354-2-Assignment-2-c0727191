d45da1997d70bdf22b3edf302b4edbf5593bf798
visualstudio <visualstudio@122-PC25>
2019-03-08 19:55:28 -0500

    lab07

--- CSD3354-2-Assignment-2-c0727191/Program.cs
+++ CSD3354-2-Assignment-2-c0727191/Program.cs
@@ -10,36 +10,35 @@ using System.Threading.Tasks;
 // March6, 2019
 namespace CSD3354_2_Assignment_2_c0727191
 {
-    class Program
+    public class program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
             Console.ReadLine();
         }
     }
-
-
     public class DelegateExercises
-
     {
-        public delegate int MyDelegate(int intValue);
+        public delegate int MyDelegate(int intvalue);
         int Method1(int intMethod1)
         {
             return intMethod1 * 2;
+            return intMethod1 * 4;
         }
-
         int Method2(int intMethod1)
         {
             return intMethod1 * 10;
+            return intMethod1 * 20;
         }
         public void Method4(MyDelegate myDelegate)
         {
             int result = myDelegate(10);
             Console.WriteLine(result);
+            for (int i = 1; i <= 5; i++)
+                System.Console.Write(myDelegate(i) + " ");
         }
-
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
@@ -49,8 +48,5 @@ namespace CSD3354_2_Assignment_2_c0727191
             Method4(myDelegate);
 
         }
-
-
     }
 }
-
