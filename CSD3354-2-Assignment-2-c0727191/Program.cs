c61c7d4102436194b509252b48ca854b811ef64d
visualstudio <visualstudio@122-PC25>
2019-03-08 21:12:02 -0500

    lab16

--- CSD3354-2-Assignment-2-c0727191/Program.cs
+++ CSD3354-2-Assignment-2-c0727191/Program.cs
@@ -16,49 +16,37 @@ namespace CSD3354_2_Assignment_2_c0727191
         public static void Main()
         {
             DelegateExercises delegateExercises = new DelegateExercises();
-            try
-            {
-                delegateExercises.Method3();
-                Console.ReadLine();
-            }
-            catch (System.Exception ex)
-            {
-                System.Console.WriteLine("Exception Occurred.");
-                Console.ReadLine();
-                delegateExercises.Method3();
-                Console.ReadLine();
-
-            }
-        }
-    }
-
-
-    public delegate void MyDelegate(ref int intValue);
-
-    public class DelegateExercises
-    {
-
-        void Method1(ref int intValue)
-        {
-            throw new System.Exception();
-            intValue = intValue + 5;
-            System.Console.WriteLine("Method1 " + intValue);
+            delegateExercises.Method3();
+            Console.ReadLine();
         }
 
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
 
-            MyDelegate myDelegate1 = new MyDelegate(Method1);
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intParameter = 5;
-            myDelegate2(ref intParameter);
-        }
     }
 }
 
 
 
+public delegate void MyDelegate(ref int intValue);
+
+public class DelegateExercises
+{
+
+    void Method1(ref int intValue)
+    {
+
+        intValue = intValue + 5;
+        System.Console.WriteLine("Method1 " + intValue);
+    }
+
+    public void Method3()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        MyDelegate myDelegate1 = new MyDelegate(Method1);
+        MyDelegate myDelegate2 = myDelegate + myDelegate1;
+        int intParameter = 5;
+        myDelegate2(ref intParameter);
+    }
+}
 
 
 
